f5ae02bff17bbb7fabaa346db4e864a6b29acf83
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-02-24 18:34:08 +0530

    initilize vectors concept

---
+++ STL/vectors.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v;
+    v.push_back(1);
+    v.emplace_back(2);
+    v.push_back(10);
+    for(int x:v) cout<<x<<" ";
+
+    
+}
