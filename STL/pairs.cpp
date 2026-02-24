3b5f18696105161d2d4472d00d4592ca5bf6a6b8
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-02-24 18:26:21 +0530

    Add pairsconcept

---
+++ STL/pairs.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<int, int>p1 = {1, 2};
+    cout << p1.first<<" "<<p1.second<<endl;
+
+    pair<int,pair<int,int>>p2 = {1,{2,3}};
+    cout << p2.first << " " << p2.second.first << " " << p2.second.second << endl;
+
+    pair<int,int>arr[] = {{1,2}, {3,4}, {5,6}};
+    cout << arr[1].first << endl;
+    
+    }
+
