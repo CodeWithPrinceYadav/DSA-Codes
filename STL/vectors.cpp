015b9572ed19dca085b9d7b21192df0f9de5bb2c
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-02-24 22:39:03 +0530

    vector iterator and print

--- STL/vectors.cpp
+++ STL/vectors.cpp
@@ -6,6 +6,29 @@ int main(){
     v.emplace_back(2);
     v.push_back(10);
     for(int x:v) cout<<x<<" ";
+    cout<<endl;
+    cout<<v[1];
 
+    vector<int>v2(5,100);
+    for(int x:v2) cout<<x<<" "; 
+    cout<<endl;  
     
+    vector<int>v3(v2);
+    for(int x:v3) cout<<x<<" ";
+    cout<<endl;
+
+    //iterators  using vector v
+    vector<int>::iterator it= v.begin();
+    it++;
+    cout<< *(it) <<endl;
+
+    it= v.end();
+    it--;
+    cout<< *(it) <<endl;
+
+    for (auto it=v.begin() ; it!=v.end() ; it++){
+        cout<< *(it)<<" ";
+    }
+    cout<<endl;
+
 }
