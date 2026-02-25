9717ae9aff7f3664049e1b047f6e28a2de8a19e4
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-02-25 17:05:28 +0530

    multi set add but there is error in code fix it

---
+++ STL/multiSet.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    multiset<int>ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(2);
+    for(auto x:ms) cout << x <<" ";
+    cout<<endl;
+    ms.erase(1);  //erase delete all 1's intend of that we use
+    for(auto x:ms) cout << x <<" ";
+    cout<<endl;
+
+    //make new multiset
+    multiset<int>ms1;
+    ms1.insert(1);
+    ms1.insert(1);
+    ms1.insert(1);
+    ms1.insert(2);
+    ms1.insert(2);
+    ms1.insert(2);
+    for(auto x:ms1) cout << x <<" ";
+    cout<<endl;
+
+    ms.erase(ms.find(1), ms.find(1)+2);
+    for(auto x:ms1) cout << x <<" ";
+    cout<<endl;
+
+}
