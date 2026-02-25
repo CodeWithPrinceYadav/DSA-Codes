81aaffafcfe1c7b78254f3c871fd3e2d358cf540
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-02-25 12:02:54 +0530

    adding list basic

---
+++ STL/list.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    list<int>ls;
+    ls.push_back(2);
+    ls.push_back(6);
+    ls.push_back(5);
+    for(auto x:ls) cout<<x<<" ";
+    cout<<endl;
+
+    ls.push_front(12);
+    ls.push_front(62);
+    ls.push_front(96);
+    for(auto x:ls) cout<<x<<" ";
+    cout<<endl;
+//rest functions are sa,\me as vector
+
+}
--- STL/vectors.cpp
+++ STL/vectors.cpp
@@ -5,9 +5,12 @@ int main(){
     v.push_back(1);
     v.emplace_back(2);
     v.push_back(10);
+    v.push_back(5);
+    v.emplace_back(12);
+    v.push_back(56);
     for(int x:v) cout<<x<<" ";
     cout<<endl;
-    cout<<v[1];
+    cout<<v[1]<<endl;
 
     vector<int>v2(5,100);
     for(int x:v2) cout<<x<<" "; 
@@ -26,9 +29,24 @@ int main(){
     it--;
     cout<< *(it) <<endl;
 
-    for (auto it=v.begin() ; it!=v.end() ; it++){
+    for (auto it=v.begin() ; it!=v.end() ; it++){  
         cout<< *(it)<<" ";
     }
     cout<<endl;
+
+    v.erase(v.begin()+1,v.begin()+5);
+    for(auto x:v) cout<<x<<" ";
+    cout<<endl;
+
+    //insert in vector
+    v.insert(v.begin()+1,2,300);
+    for(auto x:v) cout<<x<<" ";
+    cout<<endl;
+      
+    cout<<v.size()<<endl;  // size of vec
+    v.pop_back();  //pop value   it is from last
+    for(auto x:v) cout<<x<<" ";
+    cout<<endl;
+
 
 }
