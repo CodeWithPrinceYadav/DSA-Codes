baee087744fc72d41557427b650d3e582558770e
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-02-27 19:02:07 +0530

    insert deep copy

--- OOPS/shallow_and_deep.cpp
+++ OOPS/shallow_and_deep.cpp
@@ -1,6 +1,33 @@
 #include <bits/stdc++.h>
+#include <string>
 using namespace std;
 
+class student{
+    public:
+    string name;
+    double* cgpaPtr;
+        
+    student(string name, double cgpa){      //construtor 
+        this->name = name;
+    cgpaPtr = new double;       //deep copy
+        *cgpaPtr = cgpa;
+        }
+
+    student(student &obj){
+        this->name = obj.name;
+        cgpaPtr = new double;
+        *cgpaPtr = cgpa;
+    }
+    void getInfo(){
+        cout<<"Student Name: "<<name<<endl;
+        cout<<"CGPA: "<<*cgpaPtr<<endl;
+    }
+    };
+
 int main(){
-    
-}
+    student s1("Babbu Maan", 9.4);
+    s1.getInfo();
+    student s2(s1);
+    *(s2.cgpaPtr)= 7.5;
+    s2.getInfo();
+};
