24117cf2cc1037c3c5632ca209d69b6380e8f968
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-03-01 18:32:57 +0530

    adding ll

--- linkedlist/traversalInInnkedlist.cpp
+++ linkedlist/traversalInInnkedlist.cpp
@@ -10,21 +10,31 @@ struct node{
 
     node(int data1, node* next1){
         this->data = data1;
-        next = next;
+        this->next = next1;
     }
     node(int data1){
         this->data = data1;
+        this->next = nullptr;
     }
 };
 
 node* convert2ll(vector<int>arr){
     node* head = new node(arr[0]);
     node* mover = head;
-    for(int i=1; i< arr.size; i++){
-        
+    for(int i=1; i < arr.size(); i++){
+        node* temp = new node(arr[i]);
+        mover->next = temp;
+        mover= temp;
     }
-}
+    return head;
+};
 
 int main(){
-
-}
+    vector<int>arr ={9,3,9,1,56};
+    node* head = convert2ll(arr);
+    node* temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+};
