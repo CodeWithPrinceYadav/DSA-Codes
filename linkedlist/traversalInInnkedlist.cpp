080f2d6ab841d4b4fbb9b711a88286a996ad6308
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-03-01 18:24:57 +0530

    traversal in ll

---
+++ linkedlist/traversalInInnkedlist.cpp
@@ -0,0 +1,30 @@
+// traversal in linkedlist  
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+struct node{
+    public:
+    int data;
+    node* next;
+
+    node(int data1, node* next1){
+        this->data = data1;
+        next = next;
+    }
+    node(int data1){
+        this->data = data1;
+    }
+};
+
+node* convert2ll(vector<int>arr){
+    node* head = new node(arr[0]);
+    node* mover = head;
+    for(int i=1; i< arr.size; i++){
+        
+    }
+}
+
+int main(){
+
+}
