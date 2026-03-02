543104c89b44daac1393cd802505571f45dc885b
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-03-02 17:05:07 +0530

    inilize doubly lonkedlist

---
+++ linkedlist/doublyLinkedlist.cpp
@@ -0,0 +1,18 @@
+// doubly linkedList linkedlist  
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node{
+    public:
+    int data;
+    node* next;
+    node* back;
+    
+    node(int data1, node* next, node* back){
+        
+    }
+};
+
+int main(){
+
+}
