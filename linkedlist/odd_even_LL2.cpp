1d245ec2880bbb1bafba874f06aa182b863e547a
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-03-18 23:26:51 +0530

    odd even LL

---
+++ linkedlist/odd_even_LL2.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+    public:
+    int data;
+    node* next;
+
+    node(int data1, node* next1){
+        this->data = data1;
+        this->next = next1;
+    }
+    node(int data1){
+        this->data = data1;
+        this->next = nullptr;
+    }
+};
+ //conver array to ll
+node* convert2ll(vector<int>arr){
+    node* head = new node(arr[0]);
+    node* mover = head;
+    for(int i=1; i < arr.size(); i++){
+        node* temp = new node(arr[i]);
+        mover->next = temp;
+        mover= temp;
+    }
+    return head;
+};
+
+//print function
+void print(node* head){
+    while (head != NULL){
+        cout<< head->data<<" ";
+        head = head->next;
+    }
+}
+
+//odd then even Link
+node* oddEven(node* head){
+    node* odd = head;
+    node* even = head->next;
+    node* evenHead = head ->next;
+
+    while (even != NULL && even->next != NULL){
+        odd->next = odd->next->next;
+        even->next = even->next->next;
+
+        odd = odd->next;
+        even = even->next;
+    }
+    odd->next = evenHead;
+    return head;
+}
+
+int main(){
+    vector<int>arr = {10,20,30,40,50,60};
+    node* head =  convert2ll(arr);
+
+    head = oddEven(head);
+    print(head);
+    return 0;
+}
