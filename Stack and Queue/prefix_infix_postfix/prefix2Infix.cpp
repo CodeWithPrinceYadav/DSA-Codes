32109e949579af8b2c98c2a0a9d0b4f87bfcc7b3
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-04-05 23:08:55 +0530

    prefix to infix

---
+++ Stack and Queue/prefix_infix_postfix/prefix2Infix.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string prefix2Infix(string prefix){
+    stack<string> st;
+    int n = prefix.length();
+   
+    for (int i = n-1; i < prefix.length(); i--) {
+        char c = prefix[i];
+
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')){
+            st.push(string(1,prefix[i]));
+            }
+        else{
+            string t1 = st.top();
+            st.pop();
+            string t2 = st.top();
+            st.pop();
+            string con = "(" + t1 + prefix[i] +t2 + ")";
+            st.push(con);
+        }
+    }
+    return st.top();
+}
+
+int main() {
+    string exp = "*+PQ-MN";
+    cout << "prefix expression: " << exp << endl;
+    string ans = prefix2Infix(exp);
+    cout<<"Infix expression"<<ans << endl;
+    return 0;
+}
