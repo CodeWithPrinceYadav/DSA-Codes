fcbb19ef828d92bc08cb31c2beefe3920dee2d95
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-04-21 23:24:22 +0530

    add character replacement

---
+++ Sliding window and two pointer/Character_Replacement.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26, 0);
+        
+        int left = 0, right = 0;
+        int maxCount = 0;
+        int maxLength = 0;
+
+        while (right < s.size()) {
+            freq[s[right] - 'A']++;
+            maxCount = max(maxCount, freq[s[right] - 'A']);
+
+            while ((right - left + 1) - maxCount > k) {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+
+            maxLength = max(maxLength, right - left + 1);
+            right++;
+        }
+
+        return maxLength;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "AABABBA";
+    int k = 1;
+    cout << sol.characterReplacement(s, k) << endl; 
+    return 0;
+}
