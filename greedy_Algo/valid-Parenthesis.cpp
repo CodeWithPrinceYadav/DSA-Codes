9212410833dc0c101c29d50b619605ebe1f05da0
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-05-03 16:06:30 +0530

    valid parenthesis string

---
+++ greedy_Algo/valid-Parenthesis.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+bool isValid(string s){
+    int minOpen = 0;
+    int maxOpen = 0;
+    
+    for(char c: s){
+        if(c=='('){
+            minOpen++;
+            maxOpen++;
+        }
+        else if(c==')'){
+            minOpen--;
+            maxOpen--;
+        }
+        else{
+            minOpen--;
+            maxOpen++;
+        }
+        if (maxOpen < 0) return false;
+        minOpen = max(minOpen,0);
+    }
+    return minOpen==0;
+}
+
+int main(){
+    string s = "()*)*()";
+    bool ans = isValid(s);
+    cout << boolalpha << ans;
+}
