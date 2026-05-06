b153965501ae5263b64986e38a23e7fc42c944bb
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-05-06 23:49:43 +0530

    jump problem

---
+++ greedy_Algo/hard_problem/jump_problem2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0, currentEnd = 0, farthest = 0;
+
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 3, 1, 1, 4};
+
+    cout << "Minimum jumps required: " << sol.jump(nums) << endl;
+
+    return 0;
+}
