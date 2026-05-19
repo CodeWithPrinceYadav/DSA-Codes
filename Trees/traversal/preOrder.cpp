8dcf2ae27a741d95864cbe572c91f2d61c51ea7e
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-05-19 23:18:11 +0530

    inorder

--- Trees/traversal/preOrder.cpp
+++ Trees/traversal/preOrder.cpp
@@ -10,22 +10,7 @@ struct Node{
     {};
 };
 
-class Solution{
-    public:
-    void preorder(Node* root , vector<int> &arr){
-        if(root == nullptr) return ;
 
-        arr.push_back(root->data);
-        preorder(root->left, arr);
-        preorder(root->right, arr);
-    }
-
-    vector<int> preOrder(Node* root){
-        vector<int> arr;
-        preorder(root, arr);
-        return arr;
-    }
-};
 
 int main(){
     Node* root = new Node(1);
