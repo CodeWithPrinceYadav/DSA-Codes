4ee04f447972ef34f20668a004cb68c972cab2fc
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-05-20 23:21:20 +0530

    inorder

--- Trees/traversal/inorder.cpp
+++ Trees/traversal/inorder.cpp
@@ -35,6 +35,7 @@ int main() {
     root->right = new TreeNode(3);
     root->left->left = new TreeNode(4);
     root->left->right = new TreeNode(5);
+    root->left->right->right = new TreeNode(6);
 
     Solution sol;
 
