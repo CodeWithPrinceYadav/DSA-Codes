130e32a4fdffcf79d7d2839eec10ef9aeb58f796
CodeWithPrinceYadav <princeyadav15092005@gmail.com>
2026-05-26 23:16:55 +0530

    Maximum depth of binary tre

---
+++ Trees/traversal/hard_Problem/maximumDepthOfBinaryTree.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val): data(val), left(nullptr), right(nullptr)
+    {}
+};
+
+class Solution {
+public:
+    int maxdepth(Node* root){
+        if (root == NULL) return 0;
+
+        int lh = maxdepth(root->left);
+        int rh = maxdepth(root->right);
+
+        return 1 + max(lh, rh);
+    }
+};
+
+int main() {
+    Node* root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(7);
+
+    root->left->left = new Node(3);
+    root->left->right = new Node(4);
+
+    root->left->right->left = new Node(5);
+    root->left->right->right = new Node(6);
+
+    Solution sol;
+
+    int result = sol.maxdepth(root);
+
+    cout << "Maximum Depth of Binary Tree: " << result;
+
+    return 0;
+}
